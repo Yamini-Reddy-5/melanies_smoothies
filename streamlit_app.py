8632213f4bd9398a3cd532efb592af6517ea576f
YERRAPAREDDY YAMINI REDDY <122251759+Yamini-Reddy-5@users.noreply.github.com>
2026-06-12 15:25:29 +0530

    Comment out st.dataframe and st.stop calls
    Comment out dataframe display and stop functions.

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,9 +39,9 @@ my_dataframe = session.table('smoothies.public.fruit_options') \
 
 pd_df = my_dataframe.to_pandas()
 
-st.dataframe(pd_df)
+# st.dataframe(pd_df)
 
-st.stop()
+# st.stop()
 
 
 
@@ -58,6 +58,8 @@ if ingredients_list:
     for fruit_choosen in ingredients_list:
 
         ingredients_string += fruit_choosen + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_choosen + ' Nutrition Information')
 
