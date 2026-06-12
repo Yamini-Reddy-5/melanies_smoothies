ea6746e50c4fbc40d03fbeb10989308bd600b8f9
YERRAPAREDDY YAMINI REDDY <122251759+Yamini-Reddy-5@users.noreply.github.com>
2026-06-12 15:34:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,13 +43,12 @@ pd_df = my_dataframe.to_pandas()
 
 # st.stop()
 
-
-
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
-    my_dataframe,
+    pd_df['FRUIT_NAME'].tolist(),
     max_selections=5
 )
+
 if ingredients_list:
 
     ingredients_string = ''
@@ -87,6 +86,49 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is ordered!', icon='✅')
+
+# ingredients_list = st.multiselect(
+#     'Choose up to 5 ingredients:',
+#     my_dataframe,
+#     max_selections=5
+# )
+# if ingredients_list:
+
+#     ingredients_string = ''
+
+#     for fruit_chosen in ingredients_list:
+
+#         ingredients_string += fruit_chosen + ' '
+
+#         search_on = pd_df.loc[
+#             pd_df['FRUIT_NAME'] == fruit_chosen,
+#             'SEARCH_ON'
+#         ].iloc[0]
+
+#         st.subheader(fruit_chosen + ' Nutrition Information')
+
+#         smoothiefroot_response = requests.get(
+#             f"https://my.smoothiefroot.com/api/fruit/{search_on}"
+#         )
+
+#         st.dataframe(
+#             data=smoothiefroot_response.json(),
+#             use_container_width=True
+#         )
+
+#     my_insert_stmt = """insert into smoothies.public.orders
+#                     (ingredients, name_on_order)
+#                     values
+#                     ('""" + ingredients_string + """',
+#                      '""" + name_on_order + """')"""
+
+#     time_to_insert = st.button('Submit Order')
+
+#     if time_to_insert:
+
+#         session.sql(my_insert_stmt).collect()
+
+#         st.success('Your Smoothie is ordered!', icon='✅')
 
 
 
