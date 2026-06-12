7b1c52081b1d136e28e5c2c3340d7ad3a2d6a2ba
YERRAPAREDDY YAMINI REDDY <122251759+Yamini-Reddy-5@users.noreply.github.com>
2026-06-12 15:12:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ import requests
 smoothiefroot_response = requests.get(
     "https://my.smoothiefroot.com/api/fruit/watermelon"
 )
+st.text(smoothiefroot_response.json())
 
 st.text(smoothiefroot_response)
 from snowflake.snowpark.functions import col
