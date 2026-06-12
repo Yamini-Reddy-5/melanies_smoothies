e84bee9768277fe5b3e9829f650624f3f1a4917a
YERRAPAREDDY YAMINI REDDY <122251759+Yamini-Reddy-5@users.noreply.github.com>
2026-06-12 15:28:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,36 +52,29 @@ ingredients_list = st.multiselect(
 )
 if ingredients_list:
 
-    ingredients_string = ' '
+    ingredients_string = ''
 
-    for fruit_choosen in ingredients_list:
+    for fruit_chosen in ingredients_list:
 
-        ingredients_string += fruit_choosen + ' '
+        ingredients_string += fruit_chosen + ' '
 
         search_on = pd_df.loc[
-            pd_df['FRUIT_NAME'] == fruit_choosen,
+            pd_df['FRUIT_NAME'] == fruit_chosen,
             'SEARCH_ON'
         ].iloc[0]
 
-        st.write(
-            'The search value for ',
-            fruit_choosen,
-            ' is ',
-            search_on,
-            '.'
-        )
+        # st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
 
-        st.subheader(fruit_choosen + ' Nutrition Information')
+        st.subheader(fruit_chosen + ' Nutrition Information')
 
         smoothiefroot_response = requests.get(
-            "https://my.smoothiefroot.com/api/fruit/" + search_on
+            f"https://my.smoothiefroot.com/api/fruit/{search_on}"
         )
 
-        st.dataframe(
+        sf_df = st.dataframe(
             data=smoothiefroot_response.json(),
             use_container_width=True
         )
-
     my_insert_stmt = """insert into smoothies.public.orders
                     (ingredients, name_on_order)
                     values
@@ -95,6 +88,15 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is ordered!', icon='✅')
+
+
+
+
+
+
+
+
+
 # if ingredients_list:
 
 #     ingredients_string = ' '
